2ed4218ed02a6d21a40b8a0dbfe05f1d7cf97a04
lubshad <lubshadprimalcodes@gmail.com>
2021-11-02 23:51:54 +0530

    task room database added also task entity

---
+++ app/src/main/java/com/example/todomvvm/data/Task.kt
@@ -0,0 +1,20 @@
+package com.example.todomvvm.data
+
+import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.android.parcel.Parcelize
+import java.text.DateFormat
+
+
+@Entity(tableName = "task_table")
+@Parcelize
+data class Task(
+    @PrimaryKey(autoGenerate = true) val id:Int = 0,
+    val name: String,
+    val important:Boolean = false,
+    val completed:Boolean = false,
+    val createdDate: Long = System.currentTimeMillis()
+) : Parcelable {
+    val createdDateFormated: String get() = DateFormat.getDateTimeInstance().format(createdDate)
+}
