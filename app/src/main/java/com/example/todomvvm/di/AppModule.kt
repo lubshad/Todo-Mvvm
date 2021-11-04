ba2d16f16da7a1a224dfdbb6fcb2928c3b9a4c98
lubshad <lubshadprimalcodes@gmail.com>
2021-11-04 08:03:09 +0530

    dagger hilt build

--- app/src/main/java/com/example/todomvvm/di/AppModule.kt
+++ app/src/main/java/com/example/todomvvm/di/AppModule.kt
@@ -2,17 +2,19 @@ package com.example.todomvvm.di
 
 import android.app.Application
 import androidx.room.Room
-import androidx.room.RoomDatabase
 import com.example.todomvvm.data.TaskDatabase
 import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ActivityComponent
 
 
 @Module
+@InstallIn(ActivityComponent::class)
 object AppModule {
     fun provideTaskDatabase(
-        app:Application,
-        callback: TaskDatabase.Callback
-    ) : TaskDatabase {
+        app: Application,
+        callback: TaskDatabase.Callback,
+    ): TaskDatabase {
         return Room.databaseBuilder(app, TaskDatabase::class.java, "task_database")
             .fallbackToDestructiveMigration()
             .addCallback(callback)
