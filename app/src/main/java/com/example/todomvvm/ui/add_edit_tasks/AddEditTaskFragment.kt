e4e8aa009e950f2db4cdd4c0457a4d412b065a42
lubshad <lubshad4u4@gmail.com>
2021-11-14 12:33:34 +0530

    nav graph added

---
+++ app/src/main/java/com/example/todomvvm/ui/add_edit_tasks/AddEditTaskFragment.kt
@@ -0,0 +1,8 @@
+package com.example.todomvvm.ui.add_edit_tasks
+
+import androidx.fragment.app.Fragment
+import com.example.todomvvm.R
+
+class AddEditTaskFragment : Fragment(R.layout.fragment_add_edit_task) {
+
+}
