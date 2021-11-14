0881b2502d0205c48005511bbe4839444643be77
lubshad <lubshad4u4@gmail.com>
2021-11-14 14:14:27 +0530

    basic navigation adde

--- app/src/main/java/com/example/todomvvm/ui/tasks/TaskFragment.kt
+++ app/src/main/java/com/example/todomvvm/ui/tasks/TaskFragment.kt
@@ -2,23 +2,31 @@ package com.example.todomvvm.ui.tasks
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.example.todomvvm.R
 import com.example.todomvvm.databinding.FragmentTasksBinding
+import com.google.android.material.snackbar.Snackbar
+import dagger.hilt.android.AndroidEntryPoint
 
+
+@AndroidEntryPoint
 class TaskFragment : Fragment(R.layout.fragment_tasks) {
 
+    val viewModel : TaskFragmentViewModel by viewModels()
 
     private lateinit var binding: FragmentTasksBinding
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-        binding = FragmentTasksBinding.inflate(layoutInflater)
-        Log.i("TaskFragment", "fab click")
+        binding = FragmentTasksBinding.bind(view)
         binding.apply {
             fabAddTask.setOnClickListener {
-
+                val action = TaskFragmentDirections.actionTaskFragmentToAddEditTaskFragment()
+                findNavController().navigate(action)
             }
         }
     }
